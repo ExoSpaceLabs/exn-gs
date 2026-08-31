d9304275bcef1e735a574e721aa0b639ae355dd9
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-08-31 23:01:32 +0200

    fix(exn-gs): track daemon IPC connection state

--- ui/include/exn/ui/ipc_client.hpp
+++ ui/include/exn/ui/ipc_client.hpp
@@ -13,6 +13,7 @@ namespace exn::ui {
 class IpcClient {
 public:
   using OnFrame = std::function<void(const exn::proto::Frame&)>;
+  using OnConnectionState = std::function<void(bool, const std::string&)>;
 
   IpcClient(boost::asio::io_context& io, std::string host, uint16_t port);
 
@@ -21,9 +22,12 @@ public:
   void send_packet(const std::vector<uint8_t>& packet);
 
   void set_on_frame(OnFrame cb) { on_frame_ = std::move(cb); }
+  void set_on_connection_state(OnConnectionState cb) { on_connection_state_ = std::move(cb); }
 
 private:
   void do_connect();
+  void schedule_reconnect(const std::string& reason);
+  void set_connected(bool connected, const std::string& detail);
   void do_read();
   void send_frame(exn::proto::Frame frame);
   void do_write();
@@ -32,12 +36,18 @@ private:
   std::string host_;
   uint16_t port_;
 
+  boost::asio::ip::tcp::resolver resolver_;
   boost::asio::ip::tcp::socket sock_;
+  boost::asio::steady_timer reconnect_timer_;
+  bool connected_ = false;
+  bool reconnect_scheduled_ = false;
+
   std::vector<uint8_t> rxbuf_;
   std::array<uint8_t, 2048> tmp_{};
   std::deque<std::shared_ptr<std::vector<uint8_t>>> tx_queue_;
 
   OnFrame on_frame_;
+  OnConnectionState on_connection_state_;
 };
 
 } // namespace exn::ui
